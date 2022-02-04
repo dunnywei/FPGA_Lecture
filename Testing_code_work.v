88db802364eeca32cb148035186b6c447e67466e
dunnywei <wei_don@yahoo.com>
2022-02-04 14:21:56 -0700

    SPI starts

--- Testing_code_work.v
+++ Testing_code_work.v
@@ -1,4 +1,4 @@
------------------------------------start of counter--------------------------------------------------------------------
+-----------------------------------Part00:start of counter--------------------------------------------------------------------
 module Example_00(
 //    input sys_clk,
 //    output [7:0] y
@@ -19,4 +19,44 @@ module Example_00(
   //   assign y=cnt;   
 endmodule
 
------------------------------------end of counter--------------------------------------------------------------------
+-----------------------------------Part00:end of counter--------------------------------------------------------------------
+-----------------------------------Part01:Start of SPI--------------------------------------------------------------------
+module testbench;
+     reg sys_clk;
+     wire CS,MOSI,MISO;
+     
+     //DUT
+     module Example_00(sys_clk,CS,MOSI,MISO);
+    
+    integer k=0;
+    
+    initial
+    begin
+        sys_clk<=0;
+        for(k=0;k<100;k=k+1)
+        #10 sys_clk=k;  //given 100 ns
+        
+        #5 $finish;
+        
+    end
+        
+//        for(k=0;k<100;k=k+1)
+        
+endmodule
+  
+    
+module Example_00(
+     input  wire sys_clk,
+     output reg CS,
+     output wire MOSI,
+     input  wire MISO
+    );
+    
+    
+    assign MOSI=sys_clk;
+    
+    
+    
+endmodule
+-----------------------------------Part01:End of SPI--------------------------------------------------------------------
+
